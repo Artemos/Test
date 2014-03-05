6aed1f6e0310e3bcc31eec1df9d49cf71ce46bd4
Artemos <nn.magro@gmail.com>
2014-03-05 12:19:49 -0800

    Initial commit

---
+++ 2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<htmL>
+	<head>
+		<meta charset='utf-8'>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+
+	<body>
+		<div id="container">
+		<div id="header"> <img src="images/logo.jpg" alt="" id="logo">
+		<h1 id="logo-text">Your Company</h1>
+</div>
+		<div id="nav">
+			<ul>
+  				<li><a href="#">Home</a></li>
+  				<li><a href="#">About&nbsp;us</a></li>
+  				<li><a href="#">Recent&nbsp;articles</a></li>
+  				<li><a href="#">Email</a></li>
+  				<li><a href="#">Resources</a></li>
+  				<li style="border-right: medium none;"><a href="#">Links</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
---
+++ 3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<title>Title goes here</title>
+	</head>
+
+	<body>
+		<?php include("2.php") ?>
+		<div id="site-content">
+			<h1>Blablabla</h1>
+			<p class="text-1">Ceci est un test</p>
+		</div>
+		<?php include("4.php");
+		?>
+	</body>
+</html>
